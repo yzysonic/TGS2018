b86b1156643cc05e2069fdd4c56b9614a1d30b90
Umeda <bbrnobby@gmail.com>
2018-07-28 17:26:32 +0900

    タイトルシーンの編集

--- Source/SceneTitle.h
+++ Source/SceneTitle.h
@@ -1,18 +1,14 @@
 #pragma once
-#include "core\Core.h"
-#include "MainCamera.h"
+#include "Core\Core.h"
+#include "Core\Text.h"
 
 class SceneTitle :public Scene {
 public:
 	void Init(void) override;
 	void Uninit(void) override;
-
 	void Update(void) override;
 
-	FrameTimer fTimer;	// フレームタイマー
-
-
-	Object* titlelogo;
-	bool SceneEnd = false;
-
+private:
+	bool			next_scene;
+	Object*			title;
 };
