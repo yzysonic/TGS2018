b20aa68a2e47f9484813ccd4f4e0b224157ba68d
Umeda <bbrnobby@gmail.com>
2018-07-28 17:18:13 +0900

    SceneTitleの編集

--- Source/SceneTitle.cpp
+++ Source/SceneTitle.cpp
@@ -1,43 +1,40 @@
 #include "SceneTitle.h"
 #include "SceneMainGame.h"
+#include "FadeScreen.h"
 
-void SceneTitle::Init(void) {
+void SceneTitle::Init(void)
+{
+	Texture::Load("title_background");
 
-	// 背景色（背景画像と置き換え予定？）
-	Renderer::GetInstance()->getCamera()->backColor = Color(0xffffaa);
+	title = new Object;
+	title->AddComponent<RectPolygon2D>("title_background", Layer::BG_00);
+	
+	title->AddComponent<Text>()->LoadFont("おつとめフォント");
+	title->GetComponent<Text>()->SetSize(32, 0);
+	title->GetComponent<Text>()->SetColor(Color::black);
+	title->GetComponent<Text>()->area = { 0,300,800,500 };
+	title->GetComponent<Text>()->SetFormat(DT_SINGLELINE | DT_CENTER | DT_VCENTER);
 
-	//Texture::Load("background");
-	Texture::Load("titlelogo");
-
-	Object *titlelogo = new Object;
-	//Object *titlebg = new Object;
-
-	titlelogo->transform.position.x = - SystemParameters::ResolutionX * 0.1f;
-	titlelogo->transform.position.y = SystemParameters::ResolutionY * 0.2f;
-	titlelogo->AddComponent<RectPolygon2D>("titlelogo", Layer::BG_00);
-
-
-	//titlebg->transform.position.x = SystemParameters::ResolutionX * 0.5f;
-	//titlebg->AddComponent<RectPolygon2D>("background", Layer::BG_00);
-
-	fTimer.Reset(5);		// リセット（秒数で）・更新必要
-
+	next_scene = false;
+	FadeScreen::FadeIn(Color::black, 1.0f);
 }
 
-void SceneTitle::Uninit(void) {
-
-	//Texture::Release("background");
-	Texture::Release("titlelogo");
-
+void SceneTitle::Uninit(void)
+{
+	Texture::Release("title_background");
 }
 
-void SceneTitle::Update(void) {
-
-	if (GetKeyboardTrigger(DIK_RETURN))
+void SceneTitle::Update(void)
+{
+	if (!next_scene && GetKeyboardTrigger(DIK_RETURN))
 	{
-		GameManager::GetInstance()->SetScene(new SceneMainGame);
+		FadeScreen::FadeOut(Color::white, 1.0f, [this] {
+			GameManager::GetInstance()->SetScene(new SceneMainGame);
+		});
+		next_scene = true;
 	}
-
-	fTimer++;	// 秒を更新
-	fTimer.TimeUp();	// タイムアップか確認
+	if (!next_scene && FadeScreen::Finished())
+	{
+		title->GetComponent<Text>()->SetText("エンターキーではじめる");
+	}
 }
